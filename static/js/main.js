aef0c72608a784c643a833c25bfef65c76bbf43e
Joshua Tauberer <jt@occams.info>
2014-01-20 22:57:01 -0500

    parallax and fading

--- static/js/main.js
+++ static/js/main.js
@@ -9,3 +9,17 @@ $('a.smooth').click(function(e) {
 	});
 	return false;
 });
+
+$(function() {
+	var m = parseInt($('.quote').css("margin-top").replace("px", ""));
+	var h2 = $('.bigphoto').height() - 1.5*$('.quote').height() - $('.quote').offset().top;
+	if (h2 < 0) h2 = 0;
+	$(window).scroll(function() {
+		var v = $(window).scrollTop();
+		var h = $('.bigphoto').height();
+		if (v > h) v = h;
+		var m1 = m + (v/h)*h2;
+		$('.quote').css({ "margin-top": m1 + "px" });
+		$('.bigphoto').css({opacity: Math.pow(1-v/h, 2)});
+	});
+});
