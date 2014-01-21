4f2d1ae0db85887fe24fd2b222eccfadfe0e5e93
Joshua Tauberer <jt@occams.info>
2014-01-20 22:27:48 -0500

    smooth scroll

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,11 @@
-
+$('a.smooth').click(function(e) {
+	e.preventDefault();
+	var target = this.hash,
+	$target = $(target);
+	$('html, body').stop().animate({
+		'scrollTop': $target.offset().top - 15
+	}, 900, 'swing', function () {
+		//window.location.hash = target;
+	});
+	return false;
+});
